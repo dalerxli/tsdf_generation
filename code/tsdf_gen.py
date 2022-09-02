8a709f008dbb84ab9eb193a2800be9f0fdc38efe
nicoleuze <nico.leuze@gmx.de>
2022-09-02 17:55:59 +0200

    development visualization voxel grid

--- code/tsdf_gen.py
+++ code/tsdf_gen.py
@@ -1,77 +1,75 @@
+######################################################################
+# File: tsdf_gen.py
+# Project: Research Project (M.Sc.)
+# Created Date: 02.09.2022
+# Author: Nico Leuze,
+#         Faculty of Electrical Engineering and Information Technology
+#         University of Applied Science Munich (MUAS)
+# -----
+# Last Modified: 02.09.2022
+# Modified By: Nico Leuze
+# -----
+# Copyright (c) 2022 MUAS
+######################################################################
+
 from mesh_to_sdf import sample_sdf_near_surface, get_surface_point_cloud, mesh_to_voxels
-# import sys
-# sys.path.insert(1, '/home/nleuze/vgn_grasp/src/vgn/src/vgn')
-# import perception
-
-
-
 import trimesh
 import skimage
 import pyrender
+import time
 import numpy as np
+import rospy
+from sensor_msgs.msg import PointCloud2, PointField
+import std_msgs.msg, tf2_ros
 import open3d as o3d
 
-'''mesh = o3d.io.read_triangle_mesh('/home/nleuze/generate_tsdf/data/hope_meshes_eval/Ketchup.obj')
-print(mesh)
-print("Try to render a mesh with normals (exist: " +
-          str(mesh.has_vertex_normals()) + ") and colors (exist: " +
-          str(mesh.has_vertex_colors()) + ")")
-# o3d.visualization.draw_geometries([mesh])
-# print("A mesh with no normals and no colors does not seem good.")
 
-print("Computing normal and rendering it.")
-mesh.compute_vertex_normals()
-triangle_normals = np.asarray(mesh.triangle_normals)
-print(triangle_normals, triangle_normals.shape)
-o3d.visualization.draw_geometries([mesh])'''
+class TSDFVol(object):
+    def __init__(self, object='Ketchup', viz=True):
+        voxel_res = 40
+        surface_point_method = 'scan'
+        sign_method = 'normal'
+        scan_count = 100
+        scan_res = 400
+        sample_point_count = 1e6
+        return_grad = False
 
 
+        self.obj_root = '/home/nleuze/generate_tsdf/data/hope_meshes_eval/' + object + '.obj'
+        self.mesh = self.load_obj(obj_root=self.obj_root)
 
-mesh = trimesh.load('/home/nleuze/generate_tsdf/data/hope_meshes_eval/Ketchup.obj')
-print(mesh)
+        self.voxels = self.voxelize(voxel_res, scan_count, sign_method, surface_point_method, return_grad, scan_res, sample_point_count)
 
-# points, sdf = sample_sdf_near_surface(mesh, number_of_points=64000, surface_point_method='scan')
+        if viz == True:
+            ff
 
-# print('Points: ', points.shape, 'SDF: ', sdf.shape)
-# print(points[1,:])
+    def load_obj(self, obj_root):
+        return trimesh.load(obj_root)
 
-# pc = get_surface_point_cloud(mesh)
-# print('pointcloud: ', pc)
-
-# colors = np.zeros(points.shape)
-# colors[sdf < 0, 2] = 1
-# colors[sdf > 0, 0] = 1
-# cloud = pyrender.Mesh.from_points(points, colors=colors)
-# scene = pyrender.Scene()
-# scene.add(cloud)
-# viewer = pyrender.Viewer(scene, use_raymond_lighting=True, point_size=2)
+    def voxelize(self, voxel_res, scan_count, sign_method, surface_point_method, return_grad, scan_res, sample_point_count):
+        return mesh_to_voxels(self.mesh, voxel_resolution=voxel_res, check_result=False, scan_count=scan_count,
+                                     sign_method=sign_method, surface_point_method=surface_point_method, return_gradients=return_grad,
+                                     scan_resolution=scan_res, sample_point_count=sample_point_count)
 
 
-################
-# Voxelize Mesh:
-################
 
-voxels = mesh_to_voxels(mesh, 40, pad=False, surface_point_method='scan', check_result=True, scan_count=100)
+'''voxels = mesh_to_voxels(mesh, 40, pad=False, surface_point_method='scan', check_result=True, scan_count=100)
 voxels = np.asarray(voxels)
 np.savez('voxels.npz', voxels)
 
 voxels = (voxels + 1) * 0.5
 np.expand_dims(voxels, axis=0)
-print(voxels.shape)
+print(voxels.shape)'''
+
+class Visualization_Rviz(object):
+
+    def __init__(self):
+        xx = 1
 
 
-###############################
-# Initialize RVIZ Visualization
-###############################
-'''def draw_workspace(size):
-    scale = size * 0.005
-    pose = Transform.identity()
-    scale = [scale, 0.0, 0.0]
-    color = [0.5, 0.5, 0.5]
-    msg = _create_marker_msg(Marker.LINE_LIST, "task", pose, scale, color)
-    msg.points = [ros_utils.to_point_msg(point) for point in workspace_lines(size)]
-    pubs["workspace"].publish(msg)'''
 
+if __name__ == '__main__':
+    print('Starting TSDF-Generation at {}'.format(time.time()))
 
 
 
