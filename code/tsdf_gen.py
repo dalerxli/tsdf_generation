6010949447f7953427691a9530921104e0b4d418
nicoleuze <nico.leuze@gmx.de>
2022-09-10 18:22:48 +0200

    tsdf generation scipt

--- code/tsdf_gen.py
+++ code/tsdf_gen.py
@@ -23,7 +23,7 @@ import utils
 import rospy
 from sensor_msgs.msg import PointCloud2, PointField
 from visualization_msgs.msg import Marker, MarkerArray
-import scipy.spatial.transform
+import scipy
 import std_msgs.msg, tf2_ros
 import open3d as o3d
 
@@ -39,11 +39,20 @@ class TSDFVol(object):
         return_grad = False
 
 
-        self.obj_root = '/home/nico/tsdf_generation/data/hope_meshes_eval/' + object + '.obj'
+        self.obj_root = '/home/nleuze/tsdf_generation/data/hope_meshes_eval/' + object + '.obj'
         self.mesh = self.load_obj(obj_root=self.obj_root)
+        bbox = self.mesh.bounding_box.bounds
+        centroid_bbox = (bbox[0] + bbox[1]) / 2
+        # scale = (bbox[1] - bbox[0]).max()
 
         self.voxels = self.voxelize(voxel_res, scan_count, sign_method, surface_point_method, return_grad, scan_res, sample_point_count)
+        # self.voxels = (self.voxels + 1) * 0.5
+        self.trunc_dist = (0.3 / 40) * 4
+        self.voxels[self.voxels > self.trunc_dist] = self.trunc_dist
+        self.voxels[self.voxels < -self.trunc_dist] = -self.trunc_dist
+        self.voxels /= self.trunc_dist
         self.voxels = (self.voxels + 1) * 0.5
+        print('nn')
 
 
     def load_obj(self, obj_root):
@@ -63,7 +72,9 @@ class Visualization_Rviz(object):
         self.DELETE_MARKER_MSG = Marker(action=Marker.DELETEALL)
         self.DELETE_MARKER_ARRAY_MSG = MarkerArray(markers=[self.DELETE_MARKER_MSG])
         self.publishers = self.create_publishers()
-        self.clear()
+        # self.clear()
+        # self.draw_workspace()
+        # self.draw_tsdf(tsdf.get_grid().squeeze(), tsdf.voxel_size)
 
 
     def create_publishers(self):
@@ -93,16 +104,28 @@ class Visualization_Rviz(object):
         self.publishers["grasps"].publish(self.DELETE_MARKER_ARRAY_MSG)
 
     def draw_workspace(self, size=0.05):
+        # scale to dimension the lines of the workspace visualization (scale.x is used to control the width of the line segments): default 0.05 m * 0.005
         scale = size * 0.005
         # Initialize with Identity Transformation
-        rotation = utils.Rotation.from_quat([0.0, 0.0, 0.0, 1.0])
+        rotation = scipy.spatial.transform.Rotation.from_quat([0.0, 0.0, 0.0, 1.0])
         translation = np.array([0.0, 0.0, 0.0])
-        print(rotation)
+        pose = rotation, translation
         scale = [scale, 0.0, 0.0]
         color = [0.5, 0.5, 0.5]
-        msg = _create_marker_msg(Marker.LINE_LIST, "task", pose, scale, color)
-        msg.points = [utils.to_point_msg(point) for point in workspace_lines(size)]
-        self.publishers["workspace"].publish(msg)
+        msg = utils.create_marker_msg(marker_type=Marker.LINE_LIST, frame="voxel_grid_origin", pose=pose, scale=scale, color=color)
+        msg.points = [utils.to_point_msg(point) for point in utils.workspace_lines(dst=size)]
+
+        while not rospy.is_shutdown():
+            print()
+            self.publishers["workspace"].publish(msg)
+            # Set publish-rate to 10 Hz
+            rospy.sleep(0.1)
+
+    def draw_tsdf(self, vol, voxel_size, threshold=0.01):
+        print('Input Draw TSDF - Volume and Voxelsize: ', vol, voxel_size)
+        msg = utils.create_vol_msg(vol, voxel_size, threshold)
+        self.publishers["tsdf"].publish(msg)
+
 
 
 def main(args):
@@ -111,13 +134,16 @@ def main(args):
 
     if args.viz == True:
         visualization = Visualization_Rviz()
+        visualization.clear()
+        visualization.draw_workspace()
+        visualization.draw_tsdf()
 
 
 
 if __name__ == '__main__':
     print('Starting TSDF-Generation at {}\n'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
     parser = argparse.ArgumentParser()
-    parser.add_argument('--object', type=str, default='Ketchup')
+    parser.add_argument('--object', type=str, default='Ketchup_cm')
     parser.add_argument("--viz", action="store_true", default=True)
     args = parser.parse_args()
     main(args)
--- code/utils.py
+++ code/utils.py
@@ -15,6 +15,7 @@ import geometry_msgs.msg
 import numpy as np
 import rospy
 from sensor_msgs.msg import PointCloud2, PointField
+from visualization_msgs.msg import Marker, MarkerArray
 import scipy.spatial.transform
 import std_msgs.msg
 import tf2_ros
@@ -57,8 +58,107 @@ def to_cloud_msg(points, intensities=None, frame=None, stamp=None):
 
     return msg
 
+def to_point_msg(position):
+    """Convert numpy array to a Point message."""
+    msg = geometry_msgs.msg.Point()
+    msg.x = position[0]
+    msg.y = position[1]
+    msg.z = position[2]
+    return msg
+
+
+def from_point_msg(msg):
+    """Convert a Point message to a numpy array."""
+    return np.r_[msg.x, msg.y, msg.z]
+
+
+def to_vector3_msg(vector3):
+    """Convert numpy array to a Vector3 message."""
+    msg = geometry_msgs.msg.Vector3()
+    msg.x = vector3[0]
+    msg.y = vector3[1]
+    msg.z = vector3[2]
+    return msg
+
+
+def from_vector3_msg(msg):
+    """Convert a Vector3 message to a numpy array."""
+    return np.r_[msg.x, msg.y, msg.z]
+
+
+def to_quat_msg(orientation):
+    """Convert a `Rotation` object to a Quaternion message."""
+    quat = orientation.as_quat()
+    msg = geometry_msgs.msg.Quaternion()
+    msg.x = quat[0]
+    msg.y = quat[1]
+    msg.z = quat[2]
+    msg.w = quat[3]
+    return msg
+
+
+def from_quat_msg(msg):
+    """Convert a Quaternion message to a Rotation object."""
+    return Rotation.from_quat([msg.x, msg.y, msg.z, msg.w])
+
+
+def to_pose_msg(transform):
+    """Convert a `Transform` object to a Pose message."""
+    # x = transform[0].as_quat()
+    msg = geometry_msgs.msg.Pose()
+    msg.position = to_point_msg(transform[1])
+    msg.orientation = to_quat_msg(transform[0])
+    return msg
+
+
+def to_transform_msg(transform):
+    """Convert a `Transform` object to a Transform message."""
+    msg = geometry_msgs.msg.Transform()
+    msg.translation = to_vector3_msg(transform.translation)
+    msg.rotation = to_quat_msg(transform.rotation)
+    return msg
+
+
+def from_transform_msg(msg):
+    """Convert a Transform message to a Transform object."""
+    translation = from_vector3_msg(msg.translation)
+    rotation = from_quat_msg(msg.rotation)
+    return Transform(rotation, translation)
+
+
+def to_color_msg(color):
+    """Convert a numpy array to a ColorRGBA message."""
+    msg = std_msgs.msg.ColorRGBA()
+    msg.r = color[0]
+    msg.g = color[1]
+    msg.b = color[2]
+    msg.a = color[3] if len(color) == 4 else 1.0
+    return msg
+
+def create_marker_msg(marker_type, frame, pose, scale, color):
+    msg = Marker()
+    msg.header.frame_id = frame
+    msg.header.stamp = rospy.Time()
+    msg.type = marker_type
+    msg.action = Marker.ADD
+    msg.pose = to_pose_msg(pose)
+    msg.scale = to_vector3_msg(scale)
+    msg.color = to_color_msg(color)
+    return msg
+
 class Rotation(scipy.spatial.transform.Rotation):
     @classmethod
     def identity(cls):
         return cls.from_quat([0.0, 0.0, 0.0, 1.0])
+
+
+def workspace_lines(dst):
+    return [[0.0, 0.0, 0.0], [dst, 0.0, 0.0], [dst, 0.0, 0.0],
+            [dst, dst, 0.0], [dst, dst, 0.0], [0.0, dst, 0.0],
+            [0.0, dst, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, dst],
+            [dst, 0.0, dst], [dst, 0.0, dst], [dst, dst, dst],
+            [dst, dst, dst], [0.0, dst, dst], [0.0, dst, dst],
+            [0.0, 0.0, dst], [0.0, 0.0, 0.0], [0.0, 0.0, dst],
+            [dst, 0.0, 0.0], [dst, 0.0, dst], [dst, dst, 0.0],
+            [dst, dst, dst], [0.0, dst, 0.0], [0.0, dst, dst]]
 
